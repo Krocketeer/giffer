6966f46babff2bf4da803e9d240109457cf87d8e
Krocketeer <Krocketeer@github.com>
2020-07-21 11:31:03 -0700

    caught up with Evan T_T

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, {useState} from 'react'
+import './App.css'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
 function App() {
-  const [text, setText] = useState("")
-  return (
-    <div className="App">
-      <div className="searchBar">
-        <TextField id="outlined-basic" label="Search" variant="outlined"
-        color="primary" placeholder="Search for a meme"
-        />
-        <Button variant="contained" color="primary"
-          style={{height: 55, marginLeft: 8}}
-          disabled={!text}>
-          Search
+  const [text,setText] = useState('')
 
-        </Button>
+  function search(){
+
+  }
+
+  return (
+      <div className="App">
+          <div className="searchbar">
+              <TextField label="Search" variant="outlined"
+                 color="primary" placeholder="Search for a meme"
+                 value={text} onChange={e=> setText(e.target.value)}
+                 onKeyPress={e=>{
+                     if(e.key==='Enter') search()
+                 }}
+              />
+              <Button variant="contained" color="primary"
+                      style={{height:55,marginLeft:8}}
+                      disabled={!text} onClick={search}>
+                Search
+              </Button>
+            </div>
       </div>
-    </div>
   )
 }
 
